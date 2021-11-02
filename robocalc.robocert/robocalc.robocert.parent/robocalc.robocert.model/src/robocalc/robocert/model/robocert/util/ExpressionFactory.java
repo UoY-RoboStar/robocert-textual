338986056ffbf9c2b8e4f8e936d34cda9a5d625c
Matt Windsor <matt.windsor@york.ac.uk>
2021-11-02 11:53:55 +0000

    Add and test relational expressions
    We now have some rudimentary parsing tests for various types of
    expression, which have spotted a bug: we can't parse negative integers!

--- robocalc.robocert/robocalc.robocert.parent/robocalc.robocert.model/src/robocalc/robocert/model/robocert/util/ExpressionFactory.java
+++ robocalc.robocert/robocalc.robocert.parent/robocalc.robocert.model/src/robocalc/robocert/model/robocert/util/ExpressionFactory.java
@@ -2,42 +2,105 @@ package robocalc.robocert.model.robocert.util;
 
 import com.google.inject.Inject;
 
+import circus.robocalc.robochart.RoboChartFactory;
+import circus.robocalc.robochart.Variable;
+import robocalc.robocert.model.robocert.BindingExpr;
 import robocalc.robocert.model.robocert.BoolExpr;
+import robocalc.robocert.model.robocert.CertExpr;
+import robocalc.robocert.model.robocert.ConstExpr;
 import robocalc.robocert.model.robocert.IntExpr;
+import robocalc.robocert.model.robocert.RelationExpr;
+import robocalc.robocert.model.robocert.RelationOperator;
 import robocalc.robocert.model.robocert.RoboCertFactory;
 
 /**
- * Helper factory that uses a {@link RoboCertFactory} to produce specific
- * types of expression.
+ * Helper factory that uses a {@link RoboCertFactory} to produce specific types
+ * of expression.
  *
  * @author Matt Windsor
  */
 public class ExpressionFactory {
-	@Inject private RoboCertFactory rc;
-	
+	@Inject
+	private RoboCertFactory rc;
+	@Inject
+	private RoboChartFactory rchart;
+
 	/**
 	 * Creates a {@link BoolExpr} with the given truth value.
 	 * 
-	 * @param truth  the truth value.
+	 * @param truth the truth value.
 	 * 
-	 * @return  a RoboCert lifting of the given truth value.
+	 * @return a RoboCert lifting of the given truth value.
 	 */
 	public BoolExpr bool(boolean truth) {
 		var x = rc.createBoolExpr();
 		x.setTruth(truth);
 		return x;
 	}
-	
+
 	/**
 	 * Creates a {@link IntExpr} with the given value.
 	 * 
-	 * @param value  the value.
+	 * @param value the value.
 	 * 
-	 * @return  a RoboCert lifting of the given integer value.
+	 * @return a RoboCert lifting of the given integer value.
 	 */
 	public IntExpr integer(int value) {
-		var x = rc.createIntExpr();
-		x.setValue(value);
-		return x;
-	}	
+		var result = rc.createIntExpr();
+		result.setValue(value);
+		return result;
+	}
+	
+	/**
+	 * Creates a {@link ConstExpr} with a dummy {@link Variable} which, in
+	 * turn, has the given name.
+	 *
+	 * @param name the name of the variable.
+	 * 
+	 * @return the constant expression.
+	 */
+	public ConstExpr constant(String name) {
+		var k = rchart.createVariable();
+		k.setName(name);
+		
+		var result = rc.createConstExpr();
+		result.setConstant(k);
+		
+		return result;
+	}
+	
+	/**
+	 * Creates a {@link Binding} with a dummy {@link Binding} which, in
+	 * turn, has the given name.
+	 *
+	 * @param name the name of the variable.
+	 * 
+	 * @return the constant expression.
+	 */
+	public BindingExpr binding(String name) {
+		var k = rc.createBinding();
+		k.setName(name);
+		
+		var result = rc.createBindingExpr();
+		result.setSource(k);
+		
+		return result;
+	}
+
+	/**
+	 * Creates a {@link RelationExpr} with the given operator and operands.
+	 * 
+	 * @param op  the operator.
+	 * @param lhs the left operand.
+	 * @param rhs the right operand.
+	 * 
+	 * @return the given relational expression.
+	 */
+	public RelationExpr rel(RelationOperator op, CertExpr lhs, CertExpr rhs) {
+		var result = rc.createRelationExpr();
+		result.setOperator(op);
+		result.setLhs(lhs);
+		result.setRhs(rhs);
+		return result;
+	}
 }
---
+++ robocalc.robocert/robocalc.robocert.parent/robocalc.robocert.tests/src/robocalc/robocert/tests/CertExprParsingTest.java
@@ -0,0 +1,81 @@
+package robocalc.robocert.tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.StringJoiner;
+
+import org.eclipse.xtext.testing.InjectWith;
+import org.eclipse.xtext.testing.extensions.InjectionExtension;
+import org.eclipse.xtext.testing.util.ParseHelper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import com.google.inject.Inject;
+
+import robocalc.robocert.model.robocert.CertExpr;
+import robocalc.robocert.model.robocert.CertPackage;
+import robocalc.robocert.model.robocert.RelationOperator;
+import robocalc.robocert.model.robocert.util.ExpressionFactory;
+
+/**
+ * Tests the parsing of RoboCert expressions.
+ * 
+ * @author Matt Windsor
+ */
+@ExtendWith(InjectionExtension.class)
+@InjectWith(RoboCertInjectorProvider.class)
+public class CertExprParsingTest {
+	@Inject
+	private ParseHelper<CertPackage> parseHelper;
+	@Inject
+	private ExpressionFactory ef;
+
+	/**
+	 * Tests whether parsing Boolean literals works.
+	 */
+	@Test
+	void testParseBoolean() {
+		assertParse(ef.bool(true), "true");
+		assertParse(ef.bool(false), "false");
+	}
+	
+	/**
+	 * Tests whether parsing integer literals works.
+	 */
+	@Test
+	void testParseInteger() {
+		assertParse(ef.integer(0), "0");
+		// TODO(@MattWindsor91): allow negative number parsing
+		//assertParse(ef.integer(-1), "-1");
+		assertParse(ef.integer(42), "42");		
+	}
+
+	/**
+	 * Tests whether parsing integer literals works.
+	 */
+	@Test
+	void testParseRelation() {
+		assertParse(ef.rel(RelationOperator.NE, ef.integer(0), ef.integer(42)), "0 != 42");
+		assertParse(ef.rel(RelationOperator.LE, ef.binding("x"), ef.constant("K")), "@x <= K");
+		// TODO(@MattWindsor91): composite relations
+	}
+
+	
+	private void assertParse(CertExpr expected, String input) {		
+		var result = assertDoesNotThrow(() -> parseHelper.parse(lift(input)));
+		assertNotNull(result);
+		var errors = result.eResource().getErrors();
+		if (!errors.isEmpty()) {
+			var sb = new StringJoiner(", ", "Unexpected errors: ", "");
+			errors.forEach((x) -> sb.add(x.toString()));
+			fail(sb.toString());
+		}
+	}
+	
+	private String lift(String input) {
+		return """
+			sequence group X (module W -> world):
+				sequence Y:
+					<-event Z(""" + input + ")";
+	}
+}
