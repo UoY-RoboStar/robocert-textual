4a9ebd2b6a6c5375e6e979d8360cbbc4863d7c49
Matt Windsor <matt.windsor@york.ac.uk>
2021-11-10 16:28:27 +0000

    Start implementing core assertions
    Today, metamodel.  Soon, syntax and generation (but I've emailed Pedro
    with some questions about the existing generation).

---
+++ robocalc.robocert/robocalc.robocert.parent/robocalc.robocert/src/robocalc/robocert/generator/tockcsp/top/UnaryCorePropertyGenerator.java
@@ -0,0 +1,35 @@
+/********************************************************************************
+ * Copyright (c) 2021 University of York and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Pedro Ribeiro - initial definition
+ *   Matt Windsor - porting to RoboCert
+ ********************************************************************************/
+package robocalc.robocert.generator.tockcsp.top;
+
+import robocalc.robocert.model.robocert.UnaryCoreProperty;
+
+/**
+ * Generates unary 'core assertions': high level assertions such as
+ * divergence and deadlock freedom.
+ * 
+ * @author Matt Windsor
+ */
+public class UnaryCorePropertyGenerator {
+	/**
+	 * Generates CSP-M for a core property.
+	 *
+	 * @param p the property in question.
+	 *
+	 * @return generated CSP-M for a core property.
+	 */
+	public CharSequence generate(UnaryCoreProperty p) {
+		throw new UnsupportedOperationException("TODO: unimplemented");
+	}
+}
