4390834faca9864ae67d18d6b2772f3b24942986
Matt Windsor <matt.windsor@york.ac.uk>
2022-01-28 09:33:53 +0000

    Allow branchsteps and blocksteps at top of an untilfragment
    This lets us capture patterns such as 'anything until either X or Y'.
    
    The wellformedness of this is still a little unclear.  Certainly we
    can't have an untilfragment nested inside an untilfragment yet, but this
    is more for technical reasons.

--- robocalc.robocert/robocalc.robocert.parent/robocalc.robocert/src/robocalc/robocert/generator/utils/InitialSetBuilder.java
+++ robocalc.robocert/robocalc.robocert.parent/robocalc.robocert/src/robocalc/robocert/generator/utils/InitialSetBuilder.java
@@ -14,19 +14,22 @@
 package robocalc.robocert.generator.utils;
 
 import java.util.stream.Stream;
+import robocalc.robocert.model.robocert.BlockStep;
+import robocalc.robocert.model.robocert.BranchStep;
 import robocalc.robocert.model.robocert.MessageOccurrence;
 import robocalc.robocert.model.robocert.InteractionFragment;
 import robocalc.robocert.model.robocert.Message;
 import robocalc.robocert.model.robocert.OccurrenceFragment;
 import robocalc.robocert.model.robocert.Occurrence;
 import robocalc.robocert.model.robocert.Subsequence;
+import robocalc.robocert.model.robocert.UntilFragment;
 
 /**
  * Handles the construction of initial message sets for until-fragments.
  */
 public class InitialSetBuilder {
   /**
-   * Gets the initial message set for a subsequence, as a stream.
+   * Gets the initial message set for a subsequence, as a stream of message sets to be united.
    *
    * This is empty if the subsequence is empty or an occurrence other than a message, the singleton
    * set containing the message if it is a message occurrence, and will currently throw in any
@@ -40,11 +43,30 @@ public class InitialSetBuilder {
   }
 
   private Stream<Message> fragmentInitialSet(InteractionFragment fragment) {
-    // TODO(@MattWindsor91): expand this to consider more.
     if (fragment instanceof OccurrenceFragment o)
       return occurrenceInitialSet(o.getOccurrence());
 
+    // This should be caught by well-formedness, but we'll defend against it here too.
+    //
+    // (The problem is that we can't enumerate the initial set of an UntilFragment, because it
+    // could be the universe.  We might be able to relax this restriction eventually by making
+    // the InitialSetBuilder return an optimised MessageSet, but this might be tricky to make work
+    // as we don't have a parent SequenceGroup ready.)
+    if (fragment instanceof UntilFragment)
+      throw new IllegalArgumentException("can't (yet) nest UntilFragments");
+
+    // Recurse down the subsequences of combined fragments, assuming any well-formedness issues
+    // have already been dealt with.
+    if (fragment instanceof BlockStep b)
+      return initialSet(b.getBody());
+    if (fragment instanceof BranchStep b)
+      return branchInitialSet(b);
+
     throw new IllegalArgumentException("can't get initial set for fragment %s".formatted(fragment));
+  }
+
+  private Stream<Message> branchInitialSet(BranchStep b) {
+    return b.getBranches().parallelStream().flatMap((x) -> initialSet(x.getBody()));
   }
 
   private Stream<Message> occurrenceInitialSet(Occurrence occ) {
