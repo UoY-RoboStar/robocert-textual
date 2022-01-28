09f7fada9e37fbc5bbd9d83a36498b7e57cb3fa0
Matt Windsor <matt.windsor@york.ac.uk>
2022-01-28 11:13:07 +0000

    Tidy up occurrences in metamodel
    We've now committed to the erstwhile FinalAction being deadlock.
    Termination may be modelled later on, if we need it.
    
    'anything until end' is now shorthand for 'anything until: nothing',
    which doesn't seem to affect the semantics much.  For the old semantics,
    'anything until deadlock on ACTOR' still exists, but this is mainly an
    oddity I need to work out.

--- robocalc.robocert/robocalc.robocert.parent/robocalc.robocert/src/robocalc/robocert/generator/tockcsp/seq/action/OccurrenceGeneratorImpl.java
+++ robocalc.robocert/robocalc.robocert.parent/robocalc.robocert/src/robocalc/robocert/generator/tockcsp/seq/action/OccurrenceGeneratorImpl.java
@@ -19,10 +19,11 @@ import robocalc.robocert.generator.tockcsp.core.ExpressionGenerator;
 import robocalc.robocert.generator.tockcsp.ll.CSPStructureGenerator;
 import robocalc.robocert.generator.tockcsp.memory.LoadStoreGenerator;
 import robocalc.robocert.generator.tockcsp.seq.message.MessageGenerator;
+import robocalc.robocert.model.robocert.DeadlockOccurrence;
+import robocalc.robocert.model.robocert.LifelineOccurrence;
 import robocalc.robocert.model.robocert.MessageOccurrence;
-import robocalc.robocert.model.robocert.FinalAction;
 import robocalc.robocert.model.robocert.Occurrence;
-import robocalc.robocert.model.robocert.WaitAction;
+import robocalc.robocert.model.robocert.WaitOccurrence;
 
 /**
  * Top-level CSP generator for sequence actions.
@@ -50,32 +51,37 @@ public record OccurrenceGeneratorImpl(CSPStructureGenerator csp,
   /**
    * Generates CSP-M for an occurrence.
    *
-   * @param a   the occurrence.
+   * @param occ   the occurrence.
    * @param ctx context for the current lifeline.
    * @return the generated CSP.
    */
-  public CharSequence generate(Occurrence a, LifelineContext ctx) {
-    // TODO(@MattWindsor91): use lifeline context.
-
-    if (a instanceof MessageOccurrence r) {
-      return generateArrow(r);
+  public CharSequence generate(Occurrence occ, LifelineContext ctx) {
+    if (occ instanceof MessageOccurrence m) {
+      return generateMessage(m);
     }
-    // TODO(@MattWindsor91): one day, possibly more than one type of final action.
-    if (a instanceof FinalAction) {
+    if (occ instanceof LifelineOccurrence l) {
+      return l.getActor().equals(ctx.actor()) ? generateLifeline(l) : "{- not this lifeline -} SKIP";
+    }
+    throw new IllegalArgumentException("unsupported occurrence: %s".formatted(occ));
+  }
+
+  private CharSequence generateMessage(MessageOccurrence m) {
+    // TODO(@MattWindsor91): This should really be in the CSPStructureGenerator... somehow.
+    return "%s -> %sSKIP".formatted(msg.generatePrefix(m.getMessage()), lsg.generateBindingStores(m));
+  }
+
+  private CharSequence generateLifeline(LifelineOccurrence l) {
+    if (l instanceof DeadlockOccurrence) {
       return "STOP";
     }
-    if (a instanceof WaitAction w) {
+    if (l instanceof WaitOccurrence w) {
+      // TODO(@MattWindsor91): only if ctx names this actor.
       return generateWait(w);
     }
-    throw new IllegalArgumentException("unsupported sequence action: %s".formatted(a));
+    throw new IllegalArgumentException("unsupported lifeline occurrence: %s".formatted(l));
   }
 
-  private CharSequence generateArrow(MessageOccurrence r) {
-    // TODO(@MattWindsor91): This should really be in the CSPStructureGenerator... somehow.
-    return "%s -> %sSKIP".formatted(msg.generatePrefix(r.getMessage()), lsg.generateBindingStores(r));
-  }
-
-  private CharSequence generateWait(WaitAction w) {
+  private CharSequence generateWait(WaitOccurrence w) {
     // This is in the tock-CSP standard library.
     return csp.function("WAIT", eg.generate(w.getUnits()));
   }
