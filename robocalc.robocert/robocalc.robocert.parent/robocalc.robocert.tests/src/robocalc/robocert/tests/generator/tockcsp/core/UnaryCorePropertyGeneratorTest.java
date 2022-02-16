48eac94f7dae0ea27d7c80b0db9ef0917555f575
Matt Windsor <matt.windsor@york.ac.uk>
2022-02-16 11:27:50 +0000

    Make CoreProperties only take Targets
    Per discussion with Ana.  Decouples them from CSP, simplifies the
    language.
    
    Fixed tests that assumed we could put CSP in CoreProperties.
    
    There don't seem to be any places where we're using Specifications or
    CSPProcessFragments in core properties, so this restriction shouldn't
    matter.

--- robocalc.robocert/robocalc.robocert.parent/robocalc.robocert.tests/src/robocalc/robocert/tests/generator/tockcsp/core/UnaryCorePropertyGeneratorTest.java
+++ robocalc.robocert/robocalc.robocert.parent/robocalc.robocert.tests/src/robocalc/robocert/tests/generator/tockcsp/core/UnaryCorePropertyGeneratorTest.java
@@ -15,85 +15,86 @@ package robocalc.robocert.tests.generator.tockcsp.core;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static robocalc.robocert.tests.util.GeneratesCSPMatcher.generatesCSP;
 
+import circus.robocalc.robochart.RoboChartFactory;
+import com.google.inject.Inject;
 import org.eclipse.xtext.testing.InjectWith;
 import org.eclipse.xtext.testing.extensions.InjectionExtension;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-
-import com.google.inject.Inject;
-
 import robocalc.robocert.generator.tockcsp.core.CorePropertyGenerator;
+import robocalc.robocert.model.robocert.CorePropertyType;
+import robocalc.robocert.model.robocert.ModuleTarget;
 import robocalc.robocert.model.robocert.ProcessCSPFragment;
 import robocalc.robocert.model.robocert.RoboCertFactory;
-import robocalc.robocert.model.robocert.CorePropertyType;
 import robocalc.robocert.tests.util.RoboCertCustomInjectorProvider;
 
 /**
- * Tests the {@link CorePropertyGenerator} on a few properties concerning a
- * {@link ProcessCSPFragment}.
+ * Tests the {@link CorePropertyGenerator} on a few properties concerning a {@link
+ * ProcessCSPFragment}.
  *
  * @author Matt Windsor
  */
 @ExtendWith(InjectionExtension.class)
 @InjectWith(RoboCertCustomInjectorProvider.class)
 class CorePropertyGeneratorTest {
-	@Inject
-	private RoboCertFactory rc;
-	@Inject
-	private CorePropertyGenerator gen;
+  @Inject private RoboCertFactory rc;
+  @Inject private RoboChartFactory rchart;
+  @Inject private CorePropertyGenerator gen;
 
-	private ProcessCSPFragment source;
+  private ModuleTarget source;
 
-	@BeforeEach
-	void setUp() {
-		source = rc.createProcessCSPFragment();
-		source.setName("test");
-		source.setContents("STOP");
+  @BeforeEach
+  void setUp() {
+    final var mod = rchart.createRCModule();
+    mod.setName("foo");
+
+    source = rc.createModuleTarget();
+    source.setName("bar");
+    source.setModule(mod);
+
+		final var grp = rc.createTargetGroup();
+		grp.setName("baz");
+		source.setGroup(grp);
 	}
 
-	/**
-	 * Tests the generation of determinism assertions.
-	 */
-	@Test
-	void testDeterminism() {
-		assertGeneratesBody("test :[deterministic]", CorePropertyType.DETERMINISM);
-	}
+  /** Tests the generation of determinism assertions. */
+  @Test
+  void testDeterminism() {
+    assertGeneratesBody("baz::bar::Closed :[deterministic]", CorePropertyType.DETERMINISM);
+  }
 
-	/**
-	 * Tests the generation of timed deadlock freedom assertions.
-	 */
-	@Test
-	void testDeadlockFree() {
-		assertGeneratesBody(
-				"prioritise( test[[tock<-tock,tock<-tock']], <diff(Events,{tock',tock}),{tock}> )\\{tock} :[divergence free [FD]]",
-				CorePropertyType.DEADLOCK_FREE);
-	}
+  /** Tests the generation of timed deadlock freedom assertions. */
+  @Test
+  void testDeadlockFree() {
+    assertGeneratesBody(
+        "prioritise( baz::bar::Closed[[tock<-tock,tock<-tock']], <diff(Events,{tock',tock}),{tock}> )\\{tock} :[divergence free [FD]]",
+        CorePropertyType.DEADLOCK_FREE);
+  }
 
-	@Test
-	void testTimelockFree() {
-		assertGeneratesBody("RUN({tock}) ||| CHAOS(diff(Events, {|tock|})) [F= test",
-				CorePropertyType.TIMELOCK_FREE);
-	}
+  @Test
+  void testTimelockFree() {
+    assertGeneratesBody(
+        "RUN({tock}) ||| CHAOS(diff(Events, {|tock|})) [F= baz::bar::Closed", CorePropertyType.TIMELOCK_FREE);
+  }
 
-	/**
-	 * Asserts that generating positive and negative core assertions of the given
-	 * type produces, respectively, positive and negative FDR assertions with the
-	 * given expected body.
-	 *
-	 * @param expected expected body, less 'assert', 'not', and tau prioritisation.
-	 * @param type     type for which we are testing.
-	 */
-	private void assertGeneratesBody(String expected, CorePropertyType type) {
-		assertGenerates("assert %s :[tau priority]: {tock}".formatted(expected), type, false);
-		assertGenerates("assert not %s :[tau priority]: {tock}".formatted(expected), type, true);
-	}
+  /**
+   * Asserts that generating positive and negative core assertions of the given type produces,
+   * respectively, positive and negative FDR assertions with the given expected body.
+   *
+   * @param expected expected body, less 'assert', 'not', and tau prioritisation.
+   * @param type type for which we are testing.
+   */
+  private void assertGeneratesBody(String expected, CorePropertyType type) {
+    assertGenerates("assert %s :[tau priority]: {tock}".formatted(expected), type, false);
+    assertGenerates("assert not %s :[tau priority]: {tock}".formatted(expected), type, true);
+  }
 
-	private void assertGenerates(String expected, CorePropertyType type, boolean isNegated) {
-		final var p = rc.createCoreProperty();
-		p.setNegated(isNegated);
-		p.setSubject(source);
-		p.setType(type);
-		assertThat(p, generatesCSP(expected, gen::generate));
-	}
+  private void assertGenerates(String expected, CorePropertyType type, boolean isNegated) {
+    final var p = rc.createCoreProperty();
+    p.setNegated(isNegated);
+    p.setSubject(source);
+    p.setType(type);
+    assertThat(p, generatesCSP(expected, gen::generate));
+  }
 }
