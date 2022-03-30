d6433a1fff52555dc5e0ccf0fde4d3d2d00e74aa
Matt Windsor <matt.windsor@york.ac.uk>
2022-03-30 19:45:03 +0100

    Add event resolution tests
    These make me think the problem with my module logic is the scoping.

---
+++ robocalc.robocert/robocalc.robocert.parent/robocalc.robocert.tests/src/robocalc/robocert/tests/util/resolvers/EventResolverTest.java
@@ -0,0 +1,126 @@
+/*******************************************************************************
+ * Copyright (c) 2022 University of York and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Matt Windsor - initial definition
+ ******************************************************************************/
+
+package robocalc.robocert.tests.util.resolvers;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
+
+import circus.robocalc.robochart.Connection;
+import circus.robocalc.robochart.Event;
+import com.google.inject.Inject;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.eclipse.xtext.testing.InjectWith;
+import org.eclipse.xtext.testing.extensions.InjectionExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import robocalc.robocert.model.robocert.Actor;
+import robocalc.robocert.model.robocert.RoboCertFactory;
+import robocalc.robocert.model.robocert.Target;
+import robocalc.robocert.model.robocert.TargetActor;
+import robocalc.robocert.model.robocert.World;
+import robocalc.robocert.model.robocert.util.EventResolver;
+import robocalc.robocert.model.robocert.util.MessageFactory;
+import robocalc.robocert.model.robocert.util.TargetFactory;
+import robocalc.robocert.tests.RoboCertInjectorProvider;
+
+/**
+ * Tests that the {@link EventResolver} seems to be resolving things correctly on {@link
+ * ForagingExample}.
+ *
+ * @author Matt Windsor
+ */
+@ExtendWith(InjectionExtension.class)
+@InjectWith(RoboCertInjectorProvider.class)
+class EventResolverTest {
+
+  @Inject
+  private EventResolver resolver;
+  @Inject
+  private ForagingExample example;
+  @Inject
+  private RoboCertFactory certFactory;
+  @Inject
+  private MessageFactory msgFactory;
+  @Inject
+  private TargetFactory targetFactory;
+
+
+  /**
+   * Tests resolving connections on a module in the example.
+   */
+  @Test
+  void testResolve_module() {
+    final var mod = targetFactory.module(example.foraging);
+
+    final var world = certFactory.createWorld();
+    final var target = certFactory.createTargetActor();
+
+    wrapActors(mod, world, target);
+
+    final var conns1 = resolve(example.platformObstacle, example.obstacleAvoidanceObstacle, world,
+        target);
+    assertThat(conns1, hasItems(example.obstaclePlatformToObstacleAvoidance));
+
+    // This connection is not bidirectional, so this should be empty.
+    final var conns2 = resolve(example.obstacleAvoidanceObstacle, example.platformObstacle, target,
+        world);
+    assertThat(conns2, is(empty()));
+
+    // Inferring an eto.
+    final var conns3 = resolve(example.platformObstacle, null, world, target);
+    assertThat(conns3, hasItems(example.obstaclePlatformToObstacleAvoidance));
+  }
+
+  /**
+   * Tests resolving connections on a state machine in the example.
+   */
+  @Test
+  void testResolve_stateMachine() {
+    final var stm = targetFactory.stateMachine(example.avoid);
+
+    final var world = certFactory.createWorld();
+    final var target = certFactory.createTargetActor();
+
+    wrapActors(stm, world, target);
+
+    final var conns1 = resolve(example.obstacleAvoidanceObstacle, example.avoidObstacle, world,
+        target);
+    assertThat(conns1, hasItems(example.obstacleObstacleAvoidanceToAvoid));
+
+    // This connection is not bidirectional, so this should be empty.
+    final var conns2 = resolve(example.avoidObstacle, example.obstacleAvoidanceObstacle, target,
+        world);
+    assertThat(conns2, is(empty()));
+
+    // Inferring an eto.
+    final var conns3 = resolve(example.obstacleAvoidanceObstacle, null, world, target);
+    assertThat(conns3, hasItems(example.obstacleObstacleAvoidanceToAvoid));
+  }
+
+  private void wrapActors(Target t, World world, TargetActor target) {
+    final var sgroup = certFactory.createSpecificationGroup();
+    sgroup.setName("Grp");
+    sgroup.setTarget(t);
+    sgroup.getActors().add(world);
+    sgroup.getActors().add(target);
+  }
+
+  private Set<Connection> resolve(Event efrom, Event eto, Actor from, Actor to) {
+    final var topic = msgFactory.eventTopic(efrom, eto);
+    return resolver.resolve(topic, from, to).collect(Collectors.toUnmodifiableSet());
+  }
+}
--- robocalc.robocert/robocalc.robocert.parent/robocalc.robocert.tests/src/robocalc/robocert/tests/util/resolvers/ForagingExample.java
+++ robocalc.robocert/robocalc.robocert.parent/robocalc.robocert.tests/src/robocalc/robocert/tests/util/resolvers/ForagingExample.java
@@ -8,13 +8,19 @@
  * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
- *   Matt Windsor - initial definition
+ *   Edgar Buchanan, Andrew Pomfret, and Jon Timmis - initial case study
+ *   Alvaro Miyazawa, Pedro Ribeiro, Wei Li, and Ana Cavalcanti -
+ *     RoboChart conversion
+ *   Matt Windsor - this encoding
  ******************************************************************************/
 
 package robocalc.robocert.tests.util.resolvers;
 
+import circus.robocalc.robochart.Connection;
+import circus.robocalc.robochart.ConnectionNode;
 import circus.robocalc.robochart.ControllerDef;
 import circus.robocalc.robochart.Event;
+import circus.robocalc.robochart.RCModule;
 import circus.robocalc.robochart.RoboChartFactory;
 import circus.robocalc.robochart.RoboticPlatformDef;
 import circus.robocalc.robochart.StateMachineDef;
@@ -22,40 +28,80 @@ import com.google.inject.Inject;
 
 /**
  * Programmatic encoding of a simplified version of the Buchanan foraging robot case study.
+ *
+ * @author Matt Windsor
  */
 public class ForagingExample {
 
+  public Event platformObstacle;
   public Event obstacleAvoidanceObstacle;
   public Event avoidObstacle;
 
+  public RCModule foraging;
   public RoboticPlatformDef platform;
   public ControllerDef obstacleAvoidance;
   public StateMachineDef avoid;
 
+  public Connection obstaclePlatformToObstacleAvoidance;
+  public Connection obstacleObstacleAvoidanceToAvoid;
+
   @Inject
   public ForagingExample(RoboChartFactory chartFactory) {
+    platformObstacle = chartFactory.createEvent();
+    platformObstacle.setName("obstacle");
 
     avoidObstacle = chartFactory.createEvent();
     avoidObstacle.setName("obstacle");
 
-    avoid = chartFactory.createStateMachineDef();
-    avoid.setName("Avoid");
-    avoid.getEvents().add(avoidObstacle);
+    setupAvoid(chartFactory);
 
     obstacleAvoidanceObstacle = chartFactory.createEvent();
     obstacleAvoidanceObstacle.setName("obstacle");
 
+    setupObstacleAvoidance(chartFactory);
+
+    platform = chartFactory.createRoboticPlatformDef();
+    platform.setName("Platform");
+    platform.getEvents().add(platformObstacle);
+
+    obstaclePlatformToObstacleAvoidance = connection(chartFactory, platformObstacle,
+        obstacleAvoidanceObstacle, platform, obstacleAvoidance);
+
+    setupForaging(chartFactory);
+  }
+
+  private void setupForaging(RoboChartFactory chartFactory) {
+    foraging = chartFactory.createRCModule();
+    foraging.setName("Foraging");
+    foraging.getNodes().add(obstacleAvoidance);
+    foraging.getNodes().add(platform);
+    foraging.getConnections().add(obstaclePlatformToObstacleAvoidance);
+  }
+
+  private Connection connection(RoboChartFactory cf, Event efrom, Event eto, ConnectionNode from,
+      ConnectionNode to) {
+    final var result = cf.createConnection();
+    result.setEfrom(efrom);
+    result.setEto(eto);
+    result.setFrom(from);
+    result.setTo(to);
+    return result;
+  }
+
+  private void setupAvoid(RoboChartFactory chartFactory) {
+    avoid = chartFactory.createStateMachineDef();
+    avoid.setName("Avoid");
+    avoid.getEvents().add(avoidObstacle);
+  }
+
+  private void setupObstacleAvoidance(RoboChartFactory chartFactory) {
     obstacleAvoidance = chartFactory.createControllerDef();
     obstacleAvoidance.setName("ObstacleAvoidance");
     obstacleAvoidance.getEvents().add(obstacleAvoidanceObstacle);
     obstacleAvoidance.getMachines().add(avoid);
 
-    platform = chartFactory.createRoboticPlatformDef();
-    platform.setName("Platform");
-
-    final var foraging = chartFactory.createRCModule();
-    foraging.setName("Foraging");
-    foraging.getNodes().add(obstacleAvoidance);
-    foraging.getNodes().add(platform);
+    obstacleObstacleAvoidanceToAvoid = connection(chartFactory, obstacleAvoidanceObstacle,
+        avoidObstacle, obstacleAvoidance, avoid);
+    obstacleAvoidance.getConnections().add(obstacleObstacleAvoidanceToAvoid);
   }
 }
