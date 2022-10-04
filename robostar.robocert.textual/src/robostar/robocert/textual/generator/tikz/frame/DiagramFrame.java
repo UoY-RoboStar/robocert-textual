a5b6fa79a1d9b5860cf2636a28c2296cde82cd0c
Matt Windsor <matt.windsor@york.ac.uk>
2022-10-04 22:44:52 +0100

    Fix up display of diagram names
    This reduplicates a lot of Target#toString, but I'm not sure what else
    to do yet.

--- robostar.robocert.textual/src/robostar/robocert/textual/generator/tikz/frame/DiagramFrame.java
+++ robostar.robocert.textual/src/robostar/robocert/textual/generator/tikz/frame/DiagramFrame.java
@@ -13,10 +13,14 @@ package robostar.robocert.textual.generator.tikz.frame;
 import java.util.Objects;
 
 import robostar.robocert.Interaction;
+import robostar.robocert.SpecificationGroup;
+import robostar.robocert.Target;
 import robostar.robocert.textual.generator.tikz.matrix.DiagramRow;
 import robostar.robocert.textual.generator.tikz.matrix.Row;
 import robostar.robocert.textual.generator.tikz.util.InteractionFlattener.EventType;
+import robostar.robocert.textual.generator.tikz.util.TargetTypeNameGenerator;
 import robostar.robocert.textual.generator.tikz.util.TikzStructureGenerator;
+import robostar.robocert.util.resolve.TargetElementResolver;
 
 /**
  * A frame that represents the outer layer of a diagram.
@@ -32,20 +36,31 @@ public record DiagramFrame(Interaction diagram) implements Frame {
 
   @Override
   public String generateLabel(TikzStructureGenerator tikz) {
-    return tikz.command("rcseq").argument(sanitise(diagram.getName())).argument(targetName())
-        .render();
-  }
-
-  private String targetName() {
     final var group = diagram.getGroup();
-    if (group == null) {
-      return "(orphaned)";
-    }
-    final var targetBaseName = Objects.toString(group.getTarget(), "(no target)");
-    return sanitise(String.join("::", group.getName(), targetBaseName));
+    final var target = group == null ? null : group.getTarget();
+
+    // TODO(@MattWindsor91): it seems strange to construct this here.
+    final var targetType = new TargetTypeNameGenerator(tikz).targetTypeName(target);
+    final var targetName = Objects.requireNonNullElse(targetName(group, target), "???");
+
+    return tikz.command("rcseq").argument(sanitise(diagram.getName())).argument(targetType)
+        .argument(targetName).render();
   }
 
-  private String sanitise(String raw) {
+  private static String targetName(SpecificationGroup group, Target target) {
+    if (group == null || target == null) {
+      return null;
+    }
+    // TODO(@MattWindsor91): it seems strange to construct this here.
+    final var elem = new TargetElementResolver().resolve(target);
+    if (elem == null) {
+      return null;
+    }
+    final var baseName = elem.getName();
+    return baseName == null ? null : sanitise(String.join("::", group.getName(), baseName));
+  }
+
+  private static String sanitise(String raw) {
     // Needed because LaTeX usually expects _ to be in math mode.
     return raw.replace("_", "\\_");
   }
---
+++ robostar.robocert.textual/src/robostar/robocert/textual/generator/tikz/util/TargetTypeNameGenerator.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) 2022 University of York and others
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ */
+
+package robostar.robocert.textual.generator.tikz.util;
+
+import com.google.inject.Inject;
+import java.util.Objects;
+import robostar.robocert.ControllerTarget;
+import robostar.robocert.InControllerTarget;
+import robostar.robocert.InModuleTarget;
+import robostar.robocert.ModuleTarget;
+import robostar.robocert.OperationTarget;
+import robostar.robocert.StateMachineTarget;
+import robostar.robocert.Target;
+import robostar.robocert.util.RoboCertSwitch;
+
+/**
+ * Helper class for emitting the name of parts of a target.
+ *
+ * @param tikz low-level TikZ command generator.
+ * @author Matt Windsor
+ */
+public record TargetTypeNameGenerator(TikzStructureGenerator tikz) {
+  @Inject
+  public TargetTypeNameGenerator {
+    Objects.requireNonNull(tikz);
+  }
+
+  /**
+   * Gets a macro for generating the name of a target type.
+   * <p>
+   * For instance, an in-module target will produce TikZ for showing 'components of module'.
+   *
+   * @param tgt target for which we are outputting the type.
+   * @return TikZ code for displaying the type of the target.
+   */
+  public String targetTypeName(Target tgt) {
+    // TODO(@MattWindsor91): this duplicates the toString on targets!
+    final var result = new RoboCertSwitch<String>() {
+      //
+      // Component targets
+      //
+
+      @Override
+      public String caseModuleTarget(ModuleTarget m) {
+        return component("module");
+      }
+
+      @Override
+      public String caseControllerTarget(ControllerTarget m) {
+        return component("controller");
+      }
+
+
+      @Override
+      public String caseStateMachineTarget(StateMachineTarget m) {
+        return component("state machine");
+      }
+
+      @Override
+      public String caseOperationTarget(OperationTarget m) {
+        return component("operation");
+      }
+
+      //
+      // Collection targets
+      //
+
+      @Override
+      public String caseInModuleTarget(InModuleTarget m) {
+        return collection("module");
+      }
+
+      @Override
+      public String caseInControllerTarget(InControllerTarget m) {
+        return collection("controller");
+      }
+    }.doSwitch(tgt);
+    return Objects.requireNonNullElse(result, "unknown");
+  }
+
+  private String component(String name) {
+    return command("rccomptarget", name);
+  }
+
+  private String collection(String name) {
+    return command("rccolltarget", name);
+  }
+
+  private String command(String cmdName, String name) {
+    return tikz.command(cmdName).argument(name).render();
+  }
+}
