9dae99fc42dda460cbc33457da023c729ceaea0f
Matt Windsor <matt.windsor@york.ac.uk>
2022-11-02 18:42:28 +0000

    Refactor expression generator
    We now use a Switch for the main expression generator, and
    classifier lookup for the various operators.  Hopefully it works
    and I haven't broken anything.

--- robostar.robocert.textual/src/robostar/robocert/textual/generator/tockcsp/core/ExpressionGenerator.java
+++ robostar.robocert.textual/src/robostar/robocert/textual/generator/tockcsp/core/ExpressionGenerator.java
@@ -10,168 +10,149 @@
 
 package robostar.robocert.textual.generator.tockcsp.core;
 
-import circus.robocalc.robochart.And;
+import java.util.Objects;
+import java.util.Optional;
+
+import org.eclipse.emf.ecore.EObject;
+
+import com.google.inject.Inject;
+
 import circus.robocalc.robochart.BinaryExpression;
 import circus.robocalc.robochart.BooleanExp;
-import circus.robocalc.robochart.Different;
-import circus.robocalc.robochart.Div;
-import circus.robocalc.robochart.Equals;
 import circus.robocalc.robochart.Expression;
 import circus.robocalc.robochart.FloatExp;
-import circus.robocalc.robochart.GreaterOrEqual;
-import circus.robocalc.robochart.GreaterThan;
 import circus.robocalc.robochart.IntegerExp;
-import circus.robocalc.robochart.LessOrEqual;
-import circus.robocalc.robochart.LessThan;
-import circus.robocalc.robochart.Minus;
-import circus.robocalc.robochart.Modulus;
-import circus.robocalc.robochart.Mult;
 import circus.robocalc.robochart.Neg;
-import circus.robocalc.robochart.Or;
-import circus.robocalc.robochart.Plus;
 import circus.robocalc.robochart.RefExp;
+import circus.robocalc.robochart.RoboChartPackage;
 import circus.robocalc.robochart.Variable;
 import circus.robocalc.robochart.generator.csp.untimed.TypeGenerator;
 import circus.robocalc.robochart.textual.RoboCalcTypeProvider;
-import com.google.inject.Inject;
-import java.util.Objects;
-import java.util.Optional;
+import circus.robocalc.robochart.util.RoboChartSwitch;
 import robostar.robocert.textual.generator.utils.VariableHelper;
 
 /**
  * The RoboCert expression generator.
  * <p>
- * This implements (some of) the RoboChart expression language, but does so in a way with some
- * differences to how the reference expression compiler works.  The main difference is that
- * variables resolve to specification memory slots, and constants to target parameterisation.
+ * This implements (some of) the RoboChart expression language, but does so in a
+ * way with some differences to how the reference expression compiler works. The
+ * main difference is that variables resolve to specification memory slots, and
+ * constants to target parameterisation.
  * <p>
- * In the future, RoboCert may target the RoboChart generator directly. It used to in the past, but
- * this proved too complex.
+ * In the future, RoboCert may target the RoboChart generator directly. It used
+ * to in the past, but this proved too complex.
  *
  * @author Matt Windsor
  */
-public record ExpressionGenerator(TemporaryVariableGenerator bg, VariableHelper vx,
-                                  TypeGenerator typeGen, RoboCalcTypeProvider typeProvider) {
+public record ExpressionGenerator(TemporaryVariableGenerator bg, VariableHelper vx, TypeGenerator typeGen,
+		RoboCalcTypeProvider typeProvider) {
 
-  @Inject
-  public ExpressionGenerator {
-    Objects.requireNonNull(bg);
-    Objects.requireNonNull(vx);
-    Objects.requireNonNull(typeGen);
-    Objects.requireNonNull(typeProvider);
-  }
+	@Inject
+	public ExpressionGenerator {
+		Objects.requireNonNull(bg);
+		Objects.requireNonNull(vx);
+		Objects.requireNonNull(typeGen);
+		Objects.requireNonNull(typeProvider);
+	}
 
-  /**
-   * Generates CSP-M for an expression.
-   *
-   * @param it the expression to generate.
-   * @return CSP-M for the expression.
-   */
-  public CharSequence generate(Expression it) {
-    // TODO(@MattWindsor91): replace this with a type switch when it stops
-    // being a preview feature. I tried using it as a preview feature, but
-    // couldn't get the UI to load under preview mode.
-    if (it instanceof BinaryExpression b) {
-      return generateBinary(b);
-    }
-    if (it instanceof BooleanExp b) {
-      return b.getValue();
-    }
-    if (it instanceof RefExp r && r.getRef() instanceof Variable v) {
-      return generateVariable(v);
-    }
-    if (it instanceof IntegerExp i) {
-      return Integer.toString(i.getValue());
-    }
-    if (it instanceof FloatExp f) {
-      throw new IllegalArgumentException(
-          // See robochart-csp-gen#39.
-          "floating point expressions are unsupported for CSP generation: %s".formatted(f));
-    }
-    if (it instanceof Neg m) {
-      return "-(" + generate(m.getExp()) + ")";
-    }
-    throw new IllegalArgumentException(
-        "unsupported expression (only a few are supported so far): %s".formatted(it));
-  }
+	/**
+	 * Generates CSP-M for an expression.
+	 *
+	 * @param it the expression to generate.
+	 * @return CSP-M for the expression.
+	 */
+	public CharSequence generate(Expression it) {
+		return new RoboChartSwitch<CharSequence>() {
+			@Override
+			public CharSequence defaultCase(EObject e) {
+				throw new IllegalArgumentException(
+						"unsupported expression (only a few are supported so far): %s".formatted(e));
+			}
 
-  private CharSequence generateVariable(Variable v) {
-    // in RoboCert, variables are either RoboChart constants or spec-level bindings.
-    // This is the main difference between our generator and that of RoboCert.
-    return switch (v.getModifier()) {
-      case CONST -> vx.constantId(v);
-      case VAR -> bg.generateExpressionName(v);
-    };
-  }
+			@Override
+			public CharSequence caseBinaryExpression(BinaryExpression b) {
+				// TODO(@MattWindsor91): this can likely be optimised for precedence?
+				return tryGenerateArithBinary(b).orElseGet(() -> "(%s) %s (%s)".formatted(generate(b.getLeft()),
+						generateCspOp(b), generate(b.getRight())));
+			}
 
-  private CharSequence generateBinary(BinaryExpression it) {
-    // TODO(@MattWindsor91): this can likely be optimised for precedence?
-    return tryGenerateArithBinary(it).orElseGet(
-        () -> "(%s) %s (%s)".formatted(generate(it.getLeft()), generateCspOp(it),
-            generate(it.getRight())));
-  }
+			@Override
+			public CharSequence caseBooleanExp(BooleanExp b) {
+				return b.getValue();
+			}
 
-  /**
-   * Tries to see if this binary expression is an arithmetic operator; if so, expands it to the
-   * checked RoboChart definition.
-   *
-   * @param it the expression to generate.
-   * @return an Optional containing the expression; if the optional is empty, the binary expression
-   * is either handled at CSP-level or is ill-formed.
-   */
-  private Optional<CharSequence> tryGenerateArithBinary(BinaryExpression it) {
-    return generateArithOp(it).map(op -> {
-      // TODO(@MattWindsor91): type getting from a context
-      final var type = typeGen.compileType(typeProvider.typeFor(it));
-      return "%s(%s, %s, %s)".formatted(op, generate(it.getLeft()), generate(it.getRight()), type);
-    });
-  }
+			@Override
+			public CharSequence caseIntegerExp(IntegerExp i) {
+				return Integer.toString(i.getValue());
+			}
 
-  private Optional<String> generateArithOp(BinaryExpression it) {
-    if (it instanceof Plus) {
-      return Optional.of("Plus");
-    }
-    if (it instanceof Minus) {
-      return Optional.of("Minus");
-    }
-    if (it instanceof Mult) {
-      return Optional.of("Mult");
-    }
-    if (it instanceof Div) {
-      return Optional.of("Div");
-    }
-    if (it instanceof Modulus) {
-      return Optional.of("Modulus");
-    }
-    return Optional.empty();
-  }
+			@Override
+			public CharSequence caseFloatExp(FloatExp f) {
+				// See robochart-csp-gen#39.
+				throw new IllegalArgumentException(
+						"floating point expressions are unsupported for CSP generation: %s".formatted(f));
+			}
 
-  private String generateCspOp(BinaryExpression it) {
-    if (it instanceof And) {
-      return "and";
-    }
-    if (it instanceof Or) {
-      return "or";
-    }
-    if (it instanceof LessThan) {
-      return "<";
-    }
-    if (it instanceof LessOrEqual) {
-      return "<=";
-    }
-    if (it instanceof Equals) {
-      return "==";
-    }
-    if (it instanceof Different) {
-      return "!=";
-    }
-    if (it instanceof GreaterOrEqual) {
-      return ">=";
-    }
-    if (it instanceof GreaterThan) {
-      return ">";
-    }
-    throw new IllegalArgumentException(
-        "unsupported binary expression (only a few are supported so far): %s".formatted(it));
-  }
+			@Override
+			public CharSequence caseRefExp(RefExp r) {
+				if (!(r.getRef() instanceof Variable v)) {
+					return null;
+				}
+				// in RoboCert, variables are either RoboChart constants or spec-level bindings.
+				// This is the main difference between our generator and that of RoboCert.
+				return switch (v.getModifier()) {
+				case CONST -> vx.constantId(v);
+				case VAR -> bg.generateExpressionName(v);
+				};
+			}
+
+			@Override
+			public CharSequence caseNeg(Neg m) {
+				return "-(" + generate(m.getExp()) + ")";
+			}
+		}.doSwitch(it);
+	}
+
+	/**
+	 * Tries to see if this binary expression is an arithmetic operator; if so,
+	 * expands it to the checked RoboChart definition.
+	 *
+	 * @param it the expression to generate.
+	 * @return an Optional containing the expression; if the optional is empty, the
+	 *         binary expression is either handled at CSP-level or is ill-formed.
+	 */
+	private Optional<CharSequence> tryGenerateArithBinary(BinaryExpression it) {
+		return generateArithOp(it).map(op -> {
+			// TODO(@MattWindsor91): type getting from a context
+			final var type = typeGen.compileType(typeProvider.typeFor(it));
+			return "%s(%s, %s, %s)".formatted(op, generate(it.getLeft()), generate(it.getRight()), type);
+		});
+	}
+
+	private Optional<String> generateArithOp(BinaryExpression it) {
+		final var op = switch (it.eClass().getClassifierID()) {
+		case RoboChartPackage.PLUS -> "Plus";
+		case RoboChartPackage.MINUS -> "Minus";
+		case RoboChartPackage.MULT -> "Mult";
+		case RoboChartPackage.DIV -> "Div";
+		case RoboChartPackage.MODULUS -> "Modulus";
+		default -> null;
+		};
+		return Optional.ofNullable(op);
+	}
+
+	private String generateCspOp(BinaryExpression it) {
+		return switch (it.eClass().getClassifierID()) {
+		case RoboChartPackage.AND -> "and";
+		case RoboChartPackage.OR -> "or";
+		case RoboChartPackage.LESS_THAN -> "<";
+		case RoboChartPackage.LESS_OR_EQUAL -> "<=";
+		case RoboChartPackage.EQUALS -> "==";
+		case RoboChartPackage.DIFFERENT -> "!=";
+		case RoboChartPackage.GREATER_OR_EQUAL -> ">=";
+		case RoboChartPackage.GREATER_THAN -> ">";
+		default -> throw new IllegalArgumentException(
+				"unsupported binary expression (only a few are supported so far): %s".formatted(it));
+		};
+	}
 }
