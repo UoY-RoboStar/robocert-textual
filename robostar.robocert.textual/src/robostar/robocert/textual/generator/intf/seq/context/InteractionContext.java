58b0dc1a8ea32bb3e5541d20fb3059fa7b8ed33a
Matt Windsor <matt.windsor@york.ac.uk>
2023-01-16 10:09:18 +0000

    Fix context references to resolved variables
    Broken by changes to metamodel.

--- robostar.robocert.textual/src/robostar/robocert/textual/generator/intf/seq/context/InteractionContext.java
+++ robostar.robocert.textual/src/robostar/robocert/textual/generator/intf/seq/context/InteractionContext.java
@@ -13,7 +13,7 @@ package robostar.robocert.textual.generator.intf.seq.context;
 import robostar.robocert.Interaction;
 
 import java.util.stream.Stream;
-import robostar.robocert.util.resolve.VariableResolver;
+import robostar.robocert.util.resolve.result.ResolvedVariable;
 
 /**
  * Common-denominator interface for interaction contexts.
@@ -21,42 +21,43 @@ import robostar.robocert.util.resolve.VariableResolver;
  * @author Matt Windsor
  */
 public interface InteractionContext {
-    /**
-     * Gets the interaction for which this is a context.
-     *
-     * @return the interaction.
-     */
-    Interaction interaction();
 
-    /**
-     * Gets contexts for each of the lifelines in this interaction.
-     *
-     * @return a list of lifeline contexts.
-     */
-    Stream<LifelineContext> lifelines();
+  /**
+   * Gets the interaction for which this is a context.
+   *
+   * @return the interaction.
+   */
+  Interaction interaction();
 
-    /**
-     * Gets the number of lifelines in this interaction.
-     *
-     * @return the number of lifelines (equal to the size of {@code lifelines()}).
-     */
-    default long numLifelines() {
-        return lifelines().count();
-    }
+  /**
+   * Gets contexts for each of the lifelines in this interaction.
+   *
+   * @return a list of lifeline contexts.
+   */
+  Stream<LifelineContext> lifelines();
 
-    /**
-     * Gets all the variables reachable from this interaction.
-     *
-     * @return a list of variables (with attached lifelines).
-     */
-    Stream<VariableResolver.Result> variables();
+  /**
+   * Gets the number of lifelines in this interaction.
+   *
+   * @return the number of lifelines (equal to the size of {@code lifelines()}).
+   */
+  default long numLifelines() {
+    return lifelines().count();
+  }
 
-    /**
-     * Does this interaction need memory processes?
-     *
-     * @return true if, and only if, at least one specification variable exists in the interaction.
-     */
-    default boolean needsMemory() {
-        return variables().findAny().isPresent();
-    }
+  /**
+   * Gets all the variables reachable from this interaction.
+   *
+   * @return a list of resolved variables.
+   */
+  Stream<ResolvedVariable> variables();
+
+  /**
+   * Does this interaction need memory processes?
+   *
+   * @return true if, and only if, at least one specification variable exists in the interaction.
+   */
+  default boolean needsMemory() {
+    return variables().findAny().isPresent();
+  }
 }
