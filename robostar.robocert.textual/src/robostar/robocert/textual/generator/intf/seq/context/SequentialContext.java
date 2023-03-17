606cfb5517395365ebb252aa29a397a8c3ce1bee
Matt Windsor <matt.windsor@york.ac.uk>
2023-03-17 10:51:02 +0000

    Delete SpecGroup*Field
    These are effectively CSP-only.

--- robostar.robocert.textual/src/robostar/robocert/textual/generator/intf/seq/context/SequentialContext.java
+++ robostar.robocert.textual/src/robostar/robocert/textual/generator/intf/seq/context/SequentialContext.java
@@ -19,7 +19,8 @@ import robostar.robocert.Actor;
 import robostar.robocert.SequentialFragment;
 
 /**
- * A lifeline context that represents the situation while generating the body of a sequential process.
+ * A lifeline context that represents the situation while generating the body of a sequential
+ * process.
  *
  * <p>
  * In such processes, there is no separation between lifelines, whose actions are fully linearised.
@@ -31,46 +32,47 @@ import robostar.robocert.SequentialFragment;
  */
 public record SequentialContext(InteractionContext global) implements LifelineContext {
 
-    /**
-     * Constructs a sequential context.
-     *
-     * @param global the global interaction context.
-     */
-    public SequentialContext {
-        Objects.requireNonNull(global);
-    }
+  /**
+   * Constructs a sequential context.
+   *
+   * @param global the global interaction context.
+   */
+  public SequentialContext {
+    Objects.requireNonNull(global);
+  }
 
-    @Override
-    public Stream<Variable> variables() {
-        // Sequential contexts currently have no access to any variables.
-        return Stream.empty();
-    }
+  @Override
+  public Stream<Variable> variables() {
+    // Sequential contexts currently have no access to any variables.
+    return Stream.empty();
+  }
 
-    @Override
-    public boolean needsMemory() {
-        // Sequential contexts never generate a memory.
-        return false;
-    }
+  @Override
+  public boolean needsMemory() {
+    // Sequential contexts never generate a memory.
+    return false;
+  }
 
-    @Override
-    public boolean isFor(Actor a) {
-        // UntilContexts represent all lifelines simultaneously, so they are match all actors.
-        return true;
-    }
+  @Override
+  public boolean isFor(Actor a) {
+    // UntilContexts represent all lifelines simultaneously, so they are match all actors.
+    return true;
+  }
 
-    @Override
-    public boolean isForAnyOf(Stream<Actor> actors) {
-        // As above.
-        return true;
-    }
+  @Override
+  public boolean isForAnyOf(Stream<Actor> actors) {
+    // As above.
+    return true;
+  }
 
-    @Override
-    public String actorName() {
-        return "[SEQUENTIAL]";
-    }
+  @Override
+  public String actorName() {
+    return "[SEQUENTIAL]";
+  }
 
-    @Override
-    public <T> T handleSequential(SequentialFragment f, Function<SequentialFragment, T> emitInline, Function<Integer, T> emitReference) {
-        return emitInline.apply(f);
-    }
+  @Override
+  public <T> T handleSequential(SequentialFragment f, Function<SequentialFragment, T> emitInline,
+      Function<Integer, T> emitReference) {
+    return emitInline.apply(f);
+  }
 }
